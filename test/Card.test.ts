75d2e8ac441d2e4299063ed86c8fbaf75138a680
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-27 18:15:38 -0300

    Should not create a card with negative estimative

--- test/Card.test.ts
+++ test/Card.test.ts
@@ -1,9 +1,19 @@
 import Card from "../src/Card";
 
-describe("Must create a card", () => {
-    it("Must create a card", () => {
+describe("Card", () => {
+    it("Should create a card", () => {
         const card = new Card("Atividade 1", 3);
         expect(card.title).toBe("Atividade 1");
         expect(card.estimative).toBe(3);
+    });
+
+    it("Should not create a card without title", () => {
+        expect(() => new Card("", 3)).toThrow(new Error("Title is required"));
+    });
+
+    it("Should not create a card with negative estimative", () => {
+        expect(() => new Card("Atividade 1", -3)).toThrow(
+            new Error("Estimative must be positive")
+        );
     });
 });
