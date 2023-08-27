841d2ad8427a62451c9a650ff1966efe5013ed87
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-27 18:17:25 -0300

    Should not create a card without name

--- src/Column.ts
+++ src/Column.ts
@@ -1,3 +1,7 @@
 export default class Column {
-    constructor(readonly name: string, readonly hasEstimative: boolean) {}
+    constructor(readonly name: string, readonly hasEstimative: boolean) {
+        if (name === "") {
+            throw new Error("Name is required");
+        }
+    }
 }
--- test/Column.test.ts
+++ test/Column.test.ts
@@ -6,4 +6,10 @@ describe("Must create a column", () => {
         expect(column.name).toBe("Coluna A");
         expect(column.hasEstimative).toBeTruthy();
     });
+
+    it("Should not create a card without name", () => {
+        expect(() => new Column("", true)).toThrow(
+            new Error("Name is required")
+        );
+    });
 });
