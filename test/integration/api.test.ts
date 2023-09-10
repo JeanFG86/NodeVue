7c881ad66c63dec063a0547b016cadce6a55dd1f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-09-10 19:56:34 -0300

    create BoardController

--- test/integration/api.test.ts
+++ test/integration/api.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 describe("API", () => {
-    it("Should return boards via api", async () => {
+    it.only("Should return boards via api", async () => {
         const response = await axios({
             url: "http://localhost:3000/boards",
             method: "get",
