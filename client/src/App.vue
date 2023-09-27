b2e131769e30b8db0ec749cc5edd9f3e000cb0b1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-09-26 21:47:26 -0300

    create board front

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
-
+    const board = {
+        name: "Project A",
+        columns:[
+            {
+                name:"Todo",
+                cards:[]
+            },
+                {
+                name:"Doing",
+                cards:[]
+            },
+                {
+                name:"Done",
+                cards:[]
+            },
+        ]            
+    }
 </script>
 
 <template>
- <div>caraio</div>
+<div>
+    <h3>{{board.name}}</h3>
+    <div class="columns">
+        <div class="column" v-for="column in board.columns">{{column.name}}</div>
+    </div>    
+</div>
 </template>
 
-<style>
-
+<style scoped>
+    .columns{
+        display: flex;
+        flex-direction: row;
+    }
+    .column{
+        width: 200px;
+    }
 </style>
