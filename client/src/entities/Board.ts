b76724ba96bc73d44300a0baff7264ca64b08ad1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-10-31 17:18:47 -0300

    Should make a board with 3 columns

--- client/src/entities/Board.ts
+++ client/src/entities/Board.ts
@@ -1,8 +1,15 @@
+import Column from "./Column";
+
 export default class Board {
   columns: any;
   constructor(readonly name: string) {
     this.columns = [];
   }
+
+  addColumn(columnName: string, hasEstimative: boolean) {
+    this.columns.push(new Column(columnName, hasEstimative));
+  }
+
   getEstimative() {
     return 0;
   }
--- client/test/Board.test.ts
+++ client/test/Board.test.ts
@@ -5,3 +5,12 @@ test("Should make a board", function () {
   expect(board.columns).toHaveLength(0);
   expect(board.name).toBe("Projeto 1");
 });
+
+test("Should make a board with 3 columns", function () {
+  const board = new Board("Projeto 1");
+  board.addColumn("Todo", true);
+  board.addColumn("Doing", true);
+  board.addColumn("Done", false);
+  expect(board.columns).toHaveLength(3);
+  expect(board.getEstimative()).toBe(0);
+});
