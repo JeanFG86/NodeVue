03b2d161462860e72dd8b62307b08490e102ee0c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-08 20:37:46 -0300

    add saveColumn to event addColumn

--- client/src/views/BoardView.vue
+++ client/src/views/BoardView.vue
@@ -3,13 +3,16 @@ import { inject, onMounted, reactive } from "vue";
 import Board from "../entities/Board";
 import BoardService from "../services/BoardService";
 import BoardComponent from "../components/BoardComponent.vue";
+import DomainEvent from "../events/DomainEvent";
 
 const data: {board: Board | undefined} = reactive({ board: undefined});
 
 onMounted(async () => {
     const boardService = inject("boardService") as BoardService;
     const board = await boardService.getBoard(1);
-    //await boardService.saveColumn({ idBoard: 1, name: `${Math.random()}`, hasEstimative: true })
+    board.on("addColumn", async function (event: DomainEvent) {
+        await boardService.saveColumn(event.data)
+    });
     data.board = board;
 })
 
