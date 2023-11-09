f6640ba81ca14d7215e316dbf010bd30e6d3e01c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-08 21:40:10 -0300

    create AxiosAdapter & HttpClient & refactor to use

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import BoardServiceHttp from "./services/BoardServiceHttp";
 //@ts-ignore
 import App from "./App.vue";
+import AxiosAdapter from "./infra/http/AxiosAdapter";
 
 const app = createApp(App);
-app.provide("boardService", new BoardServiceHttp());
+const httpClient = new AxiosAdapter();
+app.provide("boardService", new BoardServiceHttp(httpClient));
 app.mount("#app");
