ef136a0668ccaa386ce0f8fa2875fdb73c5c0f75
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-11 10:54:01 -0300

    create FetchAdapter

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,5 +6,6 @@ import AxiosAdapter from "./infra/http/AxiosAdapter";
 
 const app = createApp(App);
 const httpClient = new AxiosAdapter();
+//const httpClient = new FetchAdapter();
 app.provide("boardService", new BoardServiceHttp(httpClient));
 app.mount("#app");
