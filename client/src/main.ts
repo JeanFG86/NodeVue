10a3a6602dea9d3d51f79b0fa24a0d87da180bf1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-12 17:32:04 -0300

    use vue-router

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,25 @@ import BoardServiceHttp from "./services/BoardServiceHttp";
 //@ts-ignore
 import App from "./App.vue";
 import AxiosAdapter from "./infra/http/AxiosAdapter";
+//@ts-ignore
+import BoardViewVue from "./views/BoardView.vue";
+//@ts-ignore
+import BoardsViewVue from "./views/BoardsView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const app = createApp(App);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/boards", component: BoardsViewVue },
+    { path: "/boards/:idBoard", component: BoardViewVue },
+  ],
+});
+
 const httpClient = new AxiosAdapter();
 //const httpClient = new FetchAdapter();
 const baseUrl = "http://localhost:3000";
+app.use(router);
 app.provide("boardService", new BoardServiceHttp(httpClient, baseUrl));
 app.mount("#app");
